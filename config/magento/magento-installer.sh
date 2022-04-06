4f2919ea964b0d3a9638e747f47183126a0eadf2
andycungkrinx <andy.setiawan@sirclo.com>
2022-04-06 23:02:20 +0700

    add reindex

--- config/magento/magento-installer.sh
+++ config/magento/magento-installer.sh
@@ -41,6 +41,7 @@ php -d memory_limit=-1 bin/magento setup:di:compile
 php -d memory_limit=-1 bin/magento setup:static-content:deploy -f
 chmod 777 -R var/ pub/ generated/
 php bin/magento maintenance:disable
+php bin/magento indexer:reindex
 php bin/magento cache:flush
 
 php-fpm7.4 -F -R;
