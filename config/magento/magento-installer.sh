edfe094673d4c01adc4503e82261ceb7c0e88e72
andycungkrinx <andy.setiawan@sirclo.com>
2022-04-06 15:30:54 +0000

    update config

--- config/magento/magento-installer.sh
+++ config/magento/magento-installer.sh
@@ -7,7 +7,7 @@ SAMPLE_DATA="/var/www/magento2/magento2-sample-data"
 composer install; \
 php bin/magento setup:install \
 --cleanup-database \
---base-url=http://localhost/ \
+--base-url=http://139.59.243.234/ \
 --db-host=database \
 --db-name=magentodb \
 --db-user=magentouser \
@@ -28,14 +28,10 @@ php bin/magento setup:install \
 
 # Config Redis+Varnish
 php bin/magento setup:config:set --http-cache-hosts=varnish:80
-php php bin/magento setup:config:set --session-save=redis --session-save-redis-host=redis-server --session-save-redis-log-level=3 --session-save-redis-db=0
+php bin/magento setup:config:set --session-save=redis --session-save-redis-host=redis-server --session-save-redis-log-level=3 --session-save-redis-db=0
 
 # Sample data
-git clone https://github.com/magento/magento2-sample-data.git
-cd $SAMPLE_DATA
-git checkout 2.4
-cd $MAGENTO_DIR
-php -f $SAMPLE_DATA/dev/tools/build-sample-data.php -- --ce-source="$MAGENTO_DIR"
+php -d memory_limit=-1 bin/magento setup:perf:generate-fixtures setup/performance-toolkit/profiles/ce/small.xml
 php -d memory_limit=-1 bin/magento setup:upgrade
 php -d memory_limit=-1 bin/magento setup:di:compile
 php -d memory_limit=-1 bin/magento setup:static-content:deploy -f
