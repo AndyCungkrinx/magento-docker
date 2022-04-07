d254fd373ce2c37df2d3671f4ce52e17d5226877
AndyCungkrinx <57337592+AndyCungkrinx@users.noreply.github.com>
2022-04-07 08:20:56 +0700

    Update composer-install.sh

--- composer-install.sh
+++ composer-install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 if [ -z "$1" ]
 then
-    docker exec -u app -it php bash -c "composer global require hirak/prestissimo && composer install"
+    docker exec -it php bash -c "composer global require hirak/prestissimo && composer install"
 else
-docker exec -u app -it php bash -c "composer global require hirak/prestissimo && composer require $1"
+docker exec -it php bash -c "composer global require hirak/prestissimo && composer require $1"
 fi
