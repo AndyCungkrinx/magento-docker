38254b3bd5a5dac0c834b7a0d290e2f1511596ea
AndyCungkrinx <57337592+AndyCungkrinx@users.noreply.github.com>
2022-04-07 08:21:20 +0700

    Update composer-update.sh

--- composer-update.sh
+++ composer-update.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 if [ -z "$1" ]
 then
-    docker exec -u app -it php bash -c "cd public_html && composer global require hirak/prestissimo && composer update no-dev"
+    docker exec -it php bash -c "cd public_html && composer global require hirak/prestissimo && composer update no-dev"
 else
-docker exec -u app -it php bash -c "cd public_html && composer global require hirak/prestissimo && composer update $1"
+docker exec -it php bash -c "cd public_html && composer global require hirak/prestissimo && composer update $1"
 fi
